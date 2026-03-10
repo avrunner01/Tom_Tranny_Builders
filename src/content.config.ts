eafdb109806e4c10a9dbc59b916644e3d973084d
avrunner01 <avrunner01@gmail.com>
2026-03-10 04:16:05 -0700

    add content.config.ts

---
+++ src/content.config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from 'astro:content';
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    tags: z.array(z.string()).optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
