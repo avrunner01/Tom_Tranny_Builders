a721b38a2a6530574af2c6982e57125084a0b7c9
avrunner01 <avrunner01@gmail.com>
2026-03-10 04:18:38 -0700

    fix build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import icon from "astro-icon";
-import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://tomtranny.netlify.app', // Moved to top level
-  output: 'static',
-  
-  devToolbar: {
-    enabled: true,
-  },
-
   integrations: [
-    react(),
     icon(),
   ],
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  output: 'server',
+  adapter: netlify(),
 });
